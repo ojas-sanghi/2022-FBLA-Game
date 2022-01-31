a4a7ee78884f41ac91d0ee941092ebfa34d94555
Rachel Dai <dai.rachell@gmail.com>
2022-01-31 10:46:05 -0700

    enemy.

--- src/actors/Actor.cs
+++ src/actors/Actor.cs
@@ -3,11 +3,11 @@ using System;
 
 public class Actor : KinematicBody2D
 {
-    protected Vector2 FLOOR_NORMAL = Vector2.Up;
-    
-    [Export] protected Vector2 speed = new Vector2(300, 1000);
-    [Export] protected int gravity = 2500;
-    [Export] protected int gravityLimit = 800;
+	protected Vector2 FLOOR_NORMAL = Vector2.Up;
+	
+	[Export] protected Vector2 speed = new Vector2(300, 1000);
+	[Export] protected int gravity = 2500;
+	[Export] protected int gravityLimit = 800;
 
-    protected Vector2 velocity = Vector2.Zero;
+	protected Vector2 velocity = Vector2.Zero;
 }
--- src/actors/Player.cs
+++ src/actors/Player.cs
@@ -3,77 +3,77 @@ using System;
 
 public class Player : Actor
 {
-    int timesJumped = 0;
-    public override void _Ready()
-    {
-        base._Ready();
-    }
+	int timesJumped = 0;
+	public override void _Ready()
+	{
+		base._Ready();
+	}
 
-    public override void _PhysicsProcess(float delta)
-    {
-        bool isJumpInterrupted = Input.IsActionJustReleased("jump") && velocity.y < 0.0;
+	public override void _PhysicsProcess(float delta)
+	{
+		bool isJumpInterrupted = Input.IsActionJustReleased("jump") && velocity.y < 0.0;
 
-        Vector2 direction = GetDirection();
-        velocity = CalculateMoveVelocity(velocity, direction, speed, isJumpInterrupted);
+		Vector2 direction = GetDirection();
+		velocity = CalculateMoveVelocity(velocity, direction, speed, isJumpInterrupted);
 
-        MoveAndSlide(velocity, FLOOR_NORMAL);
-    }
+		MoveAndSlide(velocity, FLOOR_NORMAL);
+	}
 
-    // func get_direction() -> Vector2:
+	// func get_direction() -> Vector2:
 	// return Vector2(
 	// 	Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 	// 	-1.0 if can_jump() else 1.0
 	// )
 
-    Vector2 GetDirection()
-    {
-        return new Vector2(
-            Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left"),
-            CanJump() ? -1.0f : 1.0f
-        );
-    }
+	Vector2 GetDirection()
+	{
+		return new Vector2(
+			Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left"),
+			CanJump() ? -1.0f : 1.0f
+		);
+	}
 
-    Vector2 CalculateMoveVelocity(Vector2 linearVelocity, Vector2 direction, Vector2 speed, bool isJumpInterrupted)
-    {
-        Vector2 newVel = linearVelocity;
+	Vector2 CalculateMoveVelocity(Vector2 linearVelocity, Vector2 direction, Vector2 speed, bool isJumpInterrupted)
+	{
+		Vector2 newVel = linearVelocity;
 
-        newVel.x = speed.x * direction.x;
+		newVel.x = speed.x * direction.x;
 
-        newVel.y += gravity * GetPhysicsProcessDeltaTime();
+		newVel.y += gravity * GetPhysicsProcessDeltaTime();
 
-        if (newVel.y > gravityLimit)
-        {
-            newVel.y = gravityLimit;
-        }
+		if (newVel.y > gravityLimit)
+		{
+			newVel.y = gravityLimit;
+		}
 
-        if (direction.y == -1.0f)
-        {
-            newVel.y = speed.y * direction.y;
-        }
+		if (direction.y == -1.0f)
+		{
+			newVel.y = speed.y * direction.y;
+		}
 
-        if (isJumpInterrupted)
-        {
-            newVel.y = 0.0f;
-        }
+		if (isJumpInterrupted)
+		{
+			newVel.y = 0.0f;
+		}
 
-        return newVel;
-    }
+		return newVel;
+	}
 
-    bool CanJump()
-    {
-        if (Input.IsActionJustPressed("jump"))
-        {
-            if (IsOnFloor())
-            {
-                timesJumped = 1;
-                return true;
-            }
-            if (timesJumped == 1)
-            {
-                timesJumped++;
-                return true;
-            }
-        }
-        return false;
-    }
+	bool CanJump()
+	{
+		if (Input.IsActionJustPressed("jump"))
+		{
+			if (IsOnFloor())
+			{
+				timesJumped = 1;
+				return true;
+			}
+			if (timesJumped == 1)
+			{
+				timesJumped++;
+				return true;
+			}
+		}
+		return false;
+	}
 }
