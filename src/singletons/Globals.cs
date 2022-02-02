c987da7c0b45759e450538e42c267ff39438ec6f
Ojas Sanghi <ojas.sanghi@protonmail.com>
2022-02-02 08:44:06 -0700

    highscore write and read to file

--- src/singletons/Globals.cs
+++ src/singletons/Globals.cs
@@ -1,30 +1,37 @@
-using Godot;
+using System.Linq;
 using System.Collections.Generic;
+using Godot;
+using Newtonsoft.Json;
 
 public class Globals : Node
 {
-    public static Globals Instance;
+  public static Globals Instance;
 
-    public Enums.Levels currentLevel;
-    public Dictionary<Enums.Levels, PackedScene> levels;
+  public Enums.Levels currentLevel;
+  public Dictionary<Enums.Levels, PackedScene> levels;
 
-    public Dictionary<string, int> highScores = new Dictionary<string, int>();
+  public Dictionary<string, int> highScores = new Dictionary<string, int>();
 
-    public Globals()
-    {
-        Instance = this;
-    }
+  public Globals()
+  {
+    Instance = this;
+  }
 
-    public override void _Ready()
-    {
-        Instance = this;
+  public override void _Ready()
+  {
+    Instance = this;
 
-        currentLevel = Enums.Levels.Level1;
+    currentLevel = Enums.Levels.Level1;
 
-        levels = new Dictionary<Enums.Levels, PackedScene>()
+    levels = new Dictionary<Enums.Levels, PackedScene>()
         {
           { Enums.Levels.Level1, GD.Load<PackedScene>("res://src/levels/Level1.tscn") },
           { Enums.Levels.Level2, GD.Load<PackedScene>("res://src/levels/Level2.tscn") }
         };
-    }
+
+    File scoresFile = new File();
+    scoresFile.Open("user://highscores.save", File.ModeFlags.Read);
+    highScores = JsonConvert.DeserializeObject<Dictionary<string, int>>(scoresFile.GetAsText());
+    scoresFile.Close();
+  }
 }
